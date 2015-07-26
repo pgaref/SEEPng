beb95603047ffa31c946317370b4da10cc7a7e70
pgaref <pgaref@macbook-air.lan>
2015-07-26 23:44:35 +0100

    Cassandra Data models

---
+++ examples/Collaborative-Filatering-stateful-query/src/main/java/models/PlayList.java
@@ -0,0 +1,127 @@
+package models;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.impetus.kundera.index.Index;
+import com.impetus.kundera.index.IndexCollection;
+
+//@Security.Authenticated(Secured.class)
+@Entity
+@Table(name = "playlists", schema = "play_cassandra@cassandra_pu")
+// Secondary index
+@IndexCollection(columns = { @Index(name = "usermail") })
+public class PlayList implements Serializable {
+
+	private static final long serialVersionUID = 4L;
+
+	@Id
+	public UUID id;
+
+	@Column(name = "folder")
+	public String folder;
+
+	@Column(name = "usermail")
+	public String usermail;
+
+	@ElementCollection
+	@Column(name = "tracks")
+	public List<String> tracks;
+
+	public PlayList() {
+	}
+
+	public PlayList(String usermail, String fname) {
+
+		this.id = UUID.randomUUID();
+		this.folder = fname;
+		this.usermail = usermail;
+		this.tracks = new ArrayList<String>();
+
+	}
+
+	public void addRatingSong(Track s) {
+		if (tracks == null)
+			tracks = new ArrayList<String>();
+
+		tracks.add(s.getTitle());
+	}
+
+	/**
+	 * @return the id
+	 */
+	public UUID getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the folder
+	 */
+	public String getFolder() {
+		return folder;
+	}
+
+	/**
+	 * @param folder
+	 *            the folder to set
+	 */
+	public void setFolder(String folder) {
+		this.folder = folder;
+	}
+
+	/**
+	 * @return the usermail
+	 */
+	public String getUsermail() {
+		return usermail;
+	}
+
+	/**
+	 * @param usermail
+	 *            the usermail to set
+	 */
+	public void setUsermail(String usermail) {
+		this.usermail = usermail;
+	}
+
+	/**
+	 * @return the tracks
+	 */
+	public List<String> getTracks() {
+		if(tracks == null)
+			this.tracks = new ArrayList<String>();
+		return tracks;
+	}
+
+	/**
+	 * @param tracks
+	 *            the tracks to set
+	 */
+	public void setTracks(List<String> tracks) {
+		this.tracks = tracks;
+	}
+
+	public String toString() {
+
+		return "\n--------------------------------------------------"
+				+ "\n Playlist: " + this.folder + "\n usermail: "
+				+ this.usermail + "\n Songs: "
+				+ (tracks != null ? this.tracks.toString() : " empty");
+	}
+}
---
+++ examples/Collaborative-Filatering-stateful-query/src/main/java/models/User.java
@@ -0,0 +1,130 @@
+package models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users", schema = "play_cassandra@cassandra_pu")
+//create column family users with comparator=UTF8Type and default_validation_class=UTF8Type and key_validation_class=UTF8Type;
+public class User implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name = "email")
+	public String email;
+
+	@Column(name = "username")
+	public String username;
+
+	@Column(name = "password")
+	public String password;
+
+	@Column(name = "firstname")
+	public String firstname;
+
+	@Column(name = "lastname")
+	public String lastname;	
+	
+	//default constructor
+	public User(){
+	}
+	
+	public User(String email, String username, String password) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+	}
+
+	public User(String email, String username, String password, String fname, String lname) {
+		this.email = email;
+		this.username = username;
+		this.password = password;
+		this.firstname = fname;
+		this.lastname = lname;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the firstname
+	 */
+	public String getFistname() {
+		return firstname;
+	}
+
+	/**
+	 * @param firstname the firstname to set
+	 */
+	public void setFistname(String fistname) {
+		this.firstname = fistname;
+	}
+
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * @param lastname the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	
+	public String toString() {
+		return "\n--------------------------------------------------"
+				+ "\nuserEmail: " + this.getEmail() + "\nusername: "+ this.getUsername()
+				+ "\nfirstName:" + this.getFistname() + "\nlastName: " + this.getLastname()
+				+ "\npass: " + this.password;
+	}
+	
+	
+}
